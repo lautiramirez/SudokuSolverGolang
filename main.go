1e5f914197883e0d2afb225427c906e054c93298
Lautaro <lramirez@seenka.com>
2024-12-18 16:35:16 -0300

    feat: resolve sudoku with possible values in column, row and square

--- main.go
+++ main.go
@@ -1,36 +1,51 @@
 package main
 
 import (
+	"SudokuSolverGolang/loadboard"
 	"fmt"
 	"strconv"
-	"SudokuSolverGolang/loadboard"
 )
+
 
 type Board struct {
 	High int
 	Width int 
-	Content [][]int
+	Content [][]Cell
 }
 
+
+type Cell struct {
+	Value int
+	isBlocked bool
+}
+
+
 func (board *Board) Init() {
-	board.Content = make([][]int, board.High)
+	board.Content = make([][]Cell, board.High)
 	for i := range board.Content {
-		board.Content[i] = make([]int, board.Width)
+		board.Content[i] = make([]Cell, board.Width)
 	}
 }
+
 
 func (board *Board) Print() {
 	fmt.Println("----------------------------")
 	for i := 0; i < board.High; i++ {
 		for j := 0; j < board.Width; j++ {
-			value := board.Content[i][j]
+			value := board.Content[i][j].Value
 			if j == 0 {
 				fmt.Print("| ")
 			}
 			if value == 0 {
 				fmt.Print("-")
 			} else {
-				fmt.Print(strconv.Itoa(value))
+				if (board.Content[i][j].isBlocked) {
+					fmt.Print("\033[35m")
+					fmt.Print(strconv.Itoa(value))
+					fmt.Print("\033[0m")
+				} else {
+					fmt.Print(strconv.Itoa(value))
+				}
 			}
 			if (j + 1) % 3 == 0 {
 				fmt.Print("  | ")
@@ -45,21 +60,80 @@ func (board *Board) Print() {
 	}
 }
 
+
 func (board *Board) SetValue(value int, x int, y int)(int, error){
-	if !(x < board.High && y < board.Width && 0 < value && value < board.Width) {
+	if !(x < board.High && y < board.Width && 0 < value && value <= board.Width) {
 		err := fmt.Errorf("invalid value or position: value=%d, x=%d, y=%d", value, x, y)
 		panic(err)
-	} 
-	board.Content[x][y] = value
+	} else if (board.Content[x][y].isBlocked) { 
+		err := fmt.Errorf("invalid position: x=%d, y=%d. Cell is blocked", x, y)
+		panic(err)
+	}
+	board.Content[x][y].Value = value
 	return value, nil
 }
+
 
 func (board *Board) FillBoard (valuesToFill [][]int) {
 	for i := 0; i < board.High; i++ {
 		for j := 0; j < board.Width; j++ {
-			board.Content[i][j] = valuesToFill[i][j]
+			board.Content[i][j].Value = valuesToFill[i][j]
+			if board.Content[i][j].Value == 0 {
+				board.Content[i][j].isBlocked = false
+			} else {
+				board.Content[i][j].isBlocked = true
+			}
 		}
 	}
+}
+ 
+func (board *Board) GetValuesInSameSquare (y int, x int) (map[int]struct{}) {
+	init_row := (x / 3) * 3
+	init_column := (y / 3) * 3
+	usedValues := map[int]struct{}{}
+	for i := init_row; i < init_row + 3; i++ {
+        for j := init_column; j < init_column + 3; j++ {
+			if _, ok := usedValues[board.Content[i][j].Value]; !ok {
+				if board.Content[i][j].Value != 0 {
+					usedValues[board.Content[i][j].Value] = struct{}{}
+				}
+			}
+        }
+    }
+	return usedValues
+}
+
+
+func (board *Board) GetPossibleValues(y int, x int) (map[int]struct{}) {
+	usedValues := map[int]struct{}{}
+	possibleValues := map[int]struct{}{
+		1: {}, 2: {}, 3: {},
+		4: {}, 5: {}, 6: {},
+		7: {}, 8: {}, 9: {},
+	}
+	for i := 0; i < 9; i++ {
+		if board.Content[x][i].Value != 0 {
+			if _, ok := usedValues[board.Content[x][i].Value]; !ok {
+				usedValues[board.Content[x][i].Value] = struct{}{}
+			}
+		}
+		if board.Content[i][y].Value != 0 {
+			if _, ok := usedValues[board.Content[i][y].Value]; !ok {
+				usedValues[board.Content[i][y].Value] = struct{}{}
+			}
+		}
+	}
+	inSameSquare := board.GetValuesInSameSquare(y, x)
+	for key := range inSameSquare {
+        usedValues[key] = struct{}{}
+    }
+	for key := range usedValues {
+		if _, ok := possibleValues[key]; ok {
+			delete(possibleValues, key)
+		}
+    }
+	delete(usedValues, 0)
+	return possibleValues
 }
 
 func main() {
@@ -73,17 +147,23 @@ func main() {
 	valuesToFillBoard := loadboard.GetValuesFromAPI("https://sudoku-api.vercel.app/api/dosuku")
 	board.FillBoard(valuesToFillBoard)
 	board.Print()
-	
-	var x_position int
-	var y_position int
-	var new_value int
-
-	for {
-		fmt.Printf("Enter the position (y,x) and a value to fill: ")
-		fmt.Scanln(&x_position, &y_position, &new_value)
-		board.SetValue(new_value, x_position, y_position)
-		fmt.Println()
+	for k := 0; k < 10; k++ {
+		for i := 0; i < board.High; i++ {
+			for j := 0; j < board.Width; j++ {
+				if !board.Content[i][j].isBlocked && board.Content[i][j].Value == 0 {
+					possibleValues := board.GetPossibleValues(j, i)
+					if len(possibleValues) == 1 {
+						target_value := 0
+						for key := range possibleValues {
+							target_value = key
+							break
+						}
+						board.SetValue(target_value, i, j)
+					}
+				}
+			}
+		}
+		fmt.Println("============================")
 		board.Print()
 	}
-
 }
